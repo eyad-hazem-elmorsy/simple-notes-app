723679f9507667d63c287d7e8d6e9631ebe4826a
Eyad Hazem <eyadelmorsy16@gmail.com>
2024-05-09 20:42:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+const app = express();
+
+const dbUrl = 'mongodb://localhost:27017/test';
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://localhost:27017/test')
+    .then(() => {
+        console.log('Connected to database')
+        const PORT = 4000;
+        app.listen(PORT, () => {
+            console.log('Server listen on PORT', PORT);
+        });
+    })
+    .catch(err => console.log(err.message));
+
+const noteSchema = new mongoose.Schema({
+    note: {
+        type: String,
+        required: true
+    }
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+app.get('/', async (req, res) => {
+    const notes = await Note.find({});
+    res.locals.notes = notes;
+    res.render('index');
+});
+
+app.get('/add', (req, res) => {
+    res.render('addNote');
+});
+
+app.post('/add', async (req, res) => {
+    if (!req.body.note) res.redirect('/');
+    const note = new Note({
+        note: req.body.note
+    });
+    await note.save();
+    res.redirect('/');
+});
+
+
